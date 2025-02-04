8513d60d087ea57a02e53b2209e09d44a6d3742f
LeiPinheiro <contatopinheirolei@gmail.com>
2025-02-04 14:20:44 -0300

    render server changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,15 @@ import pkg from 'pg';
 const { Pool } = pkg;
 import 'dotenv/config';
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+const pool = new pg.Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    ssl: {
+        rejectUnauthorized: false, // Importante para evitar erro de SSL no Render
+    }
 })
 
 export default pool
